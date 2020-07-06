51b4ae0b337f1d53b6da4521674db4464614889d
Diana <dianaskif@gmail.com>
2020-07-06 15:52:17 +0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Операторы +-*/
+
+console.log('arr'+'-object');//string
+console.log('4'+'-object')//string
+//console.log( 4 + + '-object') //NaN
+//Унарный плюс + + 
+console.log('4'+5)//45
+console.log(+'4'+5)
+
---
+++ script.js
@@ -0,0 +1,1 @@
+var leftBorderWight = 1;
